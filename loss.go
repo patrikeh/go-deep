45fd2f44db5109a81db441397302c16ba5aa14bb
patrikeh <patrik.ehrencrona@recaresolutions.com>
2018-01-29 16:07:52 +0100

    binary log loss correction

--- loss.go
+++ loss.go
@@ -66,15 +66,16 @@ func (l CrossEntropy) Df(estimate, ideal, activation float64) float64 {
 type BinaryCrossEntropy struct{}
 
 func (l BinaryCrossEntropy) F(estimate, ideal [][]float64) float64 {
+	epsilon := 1e-16
 	var sum float64
 	for i := range estimate {
 		ce := 0.0
 		for j := range estimate[i] {
-			ce += ideal[i][j]*math.Log(estimate[i][j]) + (1-ideal[i][j])*math.Log(1-estimate[i][j])
+			ce += ideal[i][j] * (math.Log(estimate[i][j]+epsilon) - (1-ideal[i][j])*math.Log(1-estimate[i][j]+epsilon))
 		}
 		sum -= ce
 	}
-	return sum / float64(len(estimate))
+	return sum
 }
 
 func (l BinaryCrossEntropy) Df(estimate, ideal, activation float64) float64 {
